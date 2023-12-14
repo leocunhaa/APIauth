a25892b263b6ddbfac3376c7b7f8bb9276bff99f
leonardo <leomelo.py@gmail.com>
2023-12-14 12:57:24 -0300

    Banco adicionado ao chat

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+// Conecta ao MongoDB
+mongoose.connect('mongodb://localhost:27017/AdoteJa', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define o modelo para as mensagens do chat
+const chatMessageSchema = new mongoose.Schema({
+  user: String,
+  message: String,
+});
+
+const ChatMessage = mongoose.model('ChatMessage', chatMessageSchema);
+
 // CHAT DA PAGINA HOME
 io.on('connection', (socket) => {
   console.log('Usuário conectado');
 
-  // Lógica para lidar com mensagens do servidor
+// Lógica para lidar com mensagens do servidor
 socket.on('chat', (data) => {
   if (data && data.user && data.message) {
     const { user, message } = data;
     console.log(`Mensagem do cliente: ${user}: ${message}`);
-    io.emit('chat', { user, message });  // Envia a mensagem para todos os clientes conectados
+    
+    // Salva a mensagem no banco de dados
+    const newMessage = new ChatMessage({ user, message });
+    newMessage.save();
+
+    // Envia a mensagem para todos os clientes conectados
+    io.emit('chat', { user, message });  
   }
 });
 
 
-  // Lógica para desconectar o usuário
-  socket.on('disconnect', () => {
-    console.log('Usuário desconectado');
+
+// Lógica para desconectar o usuário
+socket.on('disconnect', () => {
+  console.log('Usuário desconectado');
   });
 });
 
