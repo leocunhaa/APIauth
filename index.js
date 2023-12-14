280fa0105d68c03c4cb54b42126807ec32a5ac46
leonardo <leomelo.py@gmail.com>
2023-12-14 10:52:34 -0300

    BackEnd Chat adicionado

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-const express = require("express");
-const autenticacao = require("./middlewares/autenticacao");
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// CHAT DA PAGINA HOME
+io.on('connection', (socket) => {
+  console.log('Usuário conectado');
+
+  // Lógica para lidar com mensagens do servidor
+  socket.on('chat', (data) => {
+    const { user, message } = data;
+    console.log(`Mensagem do cliente: ${user}: ${message}`);
+    io.emit('chat', { user, message });  // Envia a mensagem para todos os clientes conectados
+  });
+
+  // Lógica para desconectar o usuário
+  socket.on('disconnect', () => {
+    console.log('Usuário desconectado');
+  });
+});
+
+
+const autenticacao = require("./middlewares/autenticacao");
 const router = require("./routes/login_criar");
 
 app.use(express.json());
@@ -14,6 +38,7 @@ app.get("/", autenticacao, (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Servidor está sendo executado na porta 3000");
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor está sendo executado na porta ${PORT}`);
 });
